22cf5e802c9b360c11159ddf7b5e6fba36f0368d
yusixian_sx <yusixian_sx@qiyi.com>
2019-03-13 18:25:43 +0800

    Structure Adjustment

--- src/_014/Test.java
+++
@@ -1,8 +0,0 @@
-package _014;
-
-public class Test {
-    public static void main(String[] args) {
-        String str = "abc";
-        System.out.println(str.substring(0, str.length() + 1));
-    }
-}
