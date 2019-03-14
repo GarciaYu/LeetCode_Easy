6879cb8efa18246836dbb427841aaef22eeaaf36
yusixian_sx <yusixian_sx@qiyi.com>
2019-03-14 09:47:13 +0800

    Delete Project

--- src/main/java/_014/Test.java
+++
@@ -1,9 +0,0 @@
-package _014;
-
-public class Test {
-    public static void main(String[] args) {
-        String str = "abc";
-        int len = str.length();
-        System.out.println(str.substring(0, len));
-    }
-}
