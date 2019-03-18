282f42e585fd5c99c710d0bf94b4a17afc0c08db
yusixian_sx <yusixian_sx@qiyi.com>
2019-03-18 19:10:01 +0800

    021

---
+++ src/main/java/_021/Main.java
@@ -0,0 +1,47 @@
+package _021;
+
+/**
+ * Merge Two Sorted Lists
+ * <p>
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together
+ * the nodes of the first two lists.
+ * <p>
+ * Example:
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        toListNodeString(l1);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        toListNodeString(l2);
+
+        Solution_Yu solution = new Solution_Yu();
+        toListNodeString(solution.mergeTwoLists(null, l2));
+    }
+
+    public static void toListNodeString(ListNode listNode) {
+        while (listNode != null) {
+            int val = listNode.val;
+            System.out.print(val + " ");
+            listNode = listNode.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/_021/Solution_Yu.java
@@ -0,0 +1,41 @@
+package _021;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution_Yu {
+    /*
+    Approach
+    Time Complexity: 86%
+    Space Complexity: 5%
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode headNode = new ListNode(0);
+        ListNode nextNode = headNode;
+        int val1 = l1 == null ? Integer.MAX_VALUE : l1.val;
+        int val2 = l2 == null ? Integer.MAX_VALUE : l2.val;
+        while (val1 != Integer.MAX_VALUE || val2 != Integer.MAX_VALUE) {
+            if (val1 < val2) {
+                nextNode.next = new ListNode(val1);
+                l1 = l1.next;
+                val1 = l1 == null ? Integer.MAX_VALUE : l1.val;
+
+            } else {
+                nextNode.next = new ListNode(val2);
+                l2 = l2.next;
+                val2 = l2 == null ? Integer.MAX_VALUE : l2.val;
+            }
+            nextNode = nextNode.next;
+        }
+        return headNode.next;
+    }
+}
